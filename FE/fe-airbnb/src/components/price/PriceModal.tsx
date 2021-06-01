57d0df8ca2528741177d0a07613ccb19723c8184
Hongbi <dyongdi@gmail.com>
2021-06-01 17:43:04 +0900

    feat: 요금 모달 UI 레이아웃
    Dae-Hwa/airbnb/#55

--- FE/fe-airbnb/src/components/price/PriceModal.tsx
+++ FE/fe-airbnb/src/components/price/PriceModal.tsx
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
+import PriceChart from './PriceChart';
+
+const PRICE_DATA = {
+  WIDTH: 365,
+  INITIAL_MIN_PRICE: 10000,
+  INITIAL_MAX_PRICE: 1000000,
+  INITIAL_LEFT: 1000,
+  INITIAL_RIGHT: 1350,
+}
 
 function PriceModal() {
   return (
     <PriceModalContainer>
-      요금 모달
+      <PriceRangeTitle>가격 범위</PriceRangeTitle>
+      <PriceRange>₩11,000 - ₩1,000,000+</PriceRange>
+      <PriceAverage>평균 1박 요금은 ₩165,556 입니다.</PriceAverage>
+      <PriceChart />
     </PriceModalContainer>
   );
 }
@@ -17,6 +29,25 @@ const PriceModalContainer = styled.div`
   margin-top: 16px;
   position: absolute;
   left: 305px;
+`
+
+const PriceRangeTitle = styled.div`
+  color: ${({theme}) => theme.colors.black};
+  font-size: ${({theme}) => theme.fontSizes.SM};
+  font-weight: bold;
+  margin-bottom: 16px;
+`
+
+const PriceRange = styled.div`
+  color: ${({theme}) => theme.colors.gray1};
+  font-size: ${({theme}) => theme.fontSizes.M};
+  margin-bottom: 4px;
+`
+
+const PriceAverage = styled.div`
+  color: ${({theme}) => theme.colors.gray3};
+  font-size: ${({theme}) => theme.fontSizes.S};
+  margin-bottom: 40px;
 `
 
 export default PriceModal;
